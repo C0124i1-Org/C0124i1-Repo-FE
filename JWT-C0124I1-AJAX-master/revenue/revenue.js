7b67487f7b63c5b147584f0366385f67137aedb5
truongphan4898 <truongphan4898@gmail.com>
2024-06-10 16:01:44 +0700

    add revenue.js

--- JWT-C0124I1-AJAX-master/revenue/revenue.js
+++ JWT-C0124I1-AJAX-master/revenue/revenue.js
@@ -23,31 +23,39 @@ function totalRevenue() {
         }
         });
 }
-function dailyRevenue(){
+function dailyRevenue() {
     let startTime = document.getElementById("start").value;
     let endTime = document.getElementById("end").value;
+
+    if (!startTime || !endTime) {
+        alert("Please select both start and end time.");
+        return;
+    }
+
     $.ajax({
         headers: {
-            "Authorization": "Bearer " + token
+            "Authorization": "Bearer " + token,
+            "Content-Type": "application/json"
         },
-        method: "GET",
+        method: "POST",
         url: "http://localhost:8080/api/revenue/time",
-        data:{
-            start:startTime,
-            end:endTime
-        },
-        success:function (data){
-            if(data != null && data.length>0){
-            let content = `
-                <table>
-                <tr>
-                <th>Name Computer</th>
-                <th>Start Time</th>
-                <th>End Time</th>
-                <th>Revenue</th>
-                </tr>
-            `;
-                data.forEach(item => {
+        data: JSON.stringify({
+            start: startTime,
+            end: endTime
+        }),
+        success: function (dulieu) {
+            if (dulieu != null && dulieu.length > 0) {
+                let totalRevenue = 0;
+                let content = `
+                    <table>
+                    <tr>
+                    <th>Name Computer</th>
+                    <th>Start Time</th>
+                    <th>End Time</th>
+                    <th>Revenue</th>
+                    </tr>
+                `;
+                dulieu.forEach(item => {
                     content += `
                     <tr>
                     <td>${item.computer.name}</td>
@@ -56,11 +64,18 @@ function dailyRevenue(){
                     <td>${item.totalRevenue}</td>
                     </tr>
                     `;
+                    totalRevenue += item.totalRevenue;
                 });
 
-            content += "</table>"
-                document.getElementById(dailyRevenue()).innerHTML=content;
+                content += `</table>
+                            <p>Total Revenue: ${totalRevenue}</p>`;
+                document.getElementById("dailyRevenue").innerHTML = content;
+            } else {
+                document.getElementById("dailyRevenue").innerHTML = "<p>No data available for the selected time range.</p>";
             }
+        },
+        error: function (xhr, status, error) {
+            console.error('Error:', status, error);
         }
-    })
-    }
+    });
+}
